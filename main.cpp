223c5b22cc229e789e64ac67198df38bb212d8c4
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2024-05-09 12:09:01 +0700

    Todo: Finsih deleteIndex

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ void horizontalLine(){
 
 struct Node{
     long long id;
-    float gpa = 0;
+    float gpa;
     std::string name;
     char gender;
     Node* next = nullptr;
@@ -29,17 +29,21 @@ struct LinkedList{
             tail = temp;
         } else{
             tail -> next = temp;
-            tail = tail -> next;
+            tail = temp;
         }
         population++;
     }
 
-    void pop_out(){
+    void deleteTail(){
         Node* current = head;
         if(population == 0){
             std::cout<<"There is no elements inside the list\n";
         } else{
-            if((current -> next)-> next == nullptr){
+            if(population == 1){
+                delete current;
+                tail = nullptr;
+                head = nullptr;
+            } else if(current->next == tail){
                 delete (current->next);
                 current -> next = nullptr;
                 tail = current;
@@ -48,18 +52,58 @@ struct LinkedList{
         }
     }
 
+    void deleteHead(){
+        Node* temp = head;
+        if(population == 0){
+            std::cout<<"There is no elements inside the list\n";
+        } else{
+            if(population == 1){
+                delete temp;
+                tail = nullptr;
+                head = nullptr;
+            } else{
+                head = head -> next;
+                delete temp;
+            }
+            population--;
+        }
+    }
+
+    void deleteIndex(int index){
+        Node* current = head;
+        if (index == 1) {
+            deleteHead();
+        } else if(index == population){
+            deleteTail();
+        } else{
+            for(int i = 1; i < index; i++) {
+                current = current->next;
+            }
+            Node* temp = current->next;
+            current->next = temp->next;
+            delete temp;
+            population--;
+        }
+    }
+
     void printComplete() const{
         Node* current = head;
-        for(int i = 1 ; i <= population ; i++){
-            std::cout<<"Index-"<<i<<" : "<<current->name<<" (NIM:"<<current->id<<')'<<std::endl;
-            if(current == tail){
-                std::cout<<"Next(NULL)\n";
-            } else{
-                std::cout<<"Next("<<(current->next)->name<<")\n";
-            }
+        if(population == 0){
+            std::cout<<"The list is empty!\n";
             horizontalLine();
-            current = current->next;
+        } else{
+            for (int i = 1; i <= population; i++) {
+                std::cout << "Index-" << i << " : " << current->name << " (NIM:" << current->id << ')' << std::endl;
+                if (current == tail) {
+                    std::cout << "Next(NULL)\n";
+                } else {
+                    std::cout << "Next(" << (current->next)->name << ")\n";
+                }
+                horizontalLine();
+                current = current->next;
+            }
         }
+        std::cout<<"Population: "<<population<<std::endl;
     }
 
     void printPartial() const {
@@ -119,28 +163,28 @@ void uppercaseGender(char& flag){
 int main() {
     //Declarations
     std::string mainMenu = "1. Add student\n2. Delete student\n3. List students\n4. Create a task\n5. Insert a grade\n6. Exit\n";
-    std::string deleteMenu = "1. Delete newest\n2. Delete oldest\n3. Delete by index\n4. Delete by name\n";
+    std::string deleteMenu = "1. Delete newest\n2. Delete oldest\n3. Delete by index\n4. Delete by name\n5. Cancel\n";
     int menuInput;
     long long idInput;
     float gpaInput;
     std::string nameInput;
     char genderInput;
-    bool programIsRunning{true},notValid{true};
+    bool programIsRunning{true},notValid{true}, deletionIsRunning{true};
     LinkedList test;
 
     //Program Running Event Loops
     do{
         std::cout<<mainMenu<<"Input:\n";
         std::cin>>menuInput;
+        std::cin.ignore(1, '\n');
         horizontalLine();
         switch(menuInput){
 
             //Add student
             case 1:
                 //Name
-                while(notValid){
+                do{
                     std::cout << "Enter the name of the student:\n";
-                    std::cin.ignore(1, '\n');
                     std::getline(std::cin, nameInput);
                     horizontalLine();
                     if(test.duplicateName(nameInput)){
@@ -149,10 +193,11 @@ int main() {
                     } else{
                         notValid = false;
                     }
-                }
+                }while(notValid);
                 //ID
                 do{
                     std::cout<<"Enter the student ID:\n";
+                    horizontalLine();
                     std::cin>>idInput;
                     horizontalLine();
                     if(idInput < 1){
@@ -191,12 +236,56 @@ int main() {
                 test.push_back(idInput, gpaInput , nameInput, genderInput);
                 notValid = true;
                 break;
+
+            //Delete student
             case 2:
-                //
+                if(test.population == 0){
+                    std::cout<<"There is no student to delete\n";
+                } else{
+                    do {
+                        std::cout << deleteMenu;
+                        horizontalLine();
+                        std::cin >> menuInput;
+                        horizontalLine();
+                        switch (menuInput) {
+                            case 1:
+                                //Delete most recent added
+                                test.deleteTail();
+                                break;
+                            case 2:
+                                //Delete most oldest added
+                                test.deleteHead();
+                                break;
+                            case 3:
+                                //Delete by index
+                                do {
+                                    std::cout << "Which roll number do you want to delete?\n";
+                                    std::cin >> menuInput;
+                                    if (test.population < menuInput && menuInput > 1) {
+                                        std::cout << "There is no student of that roll number\n";
+                                    } else {
+                                        test.deleteIndex(menuInput);
+                                        notValid = false;
+                                    }
+                                } while (notValid);
+                                break;
+                            case 4:
+                                //Delete by name
+                                break;
+                            case 5:
+                                deletionIsRunning = false;
+                                break;
+                            default:
+                                std::cout
+                                        << "The value you just entered is beyond our instruction, please try again.\n";
+                                break;
+                        }
+                    } while (deletionIsRunning);
+                    deletionIsRunning = true;
+                }
                 break;
             case 3:
                 test.printComplete();
-                horizontalLine();
                 break;
             case 4:
                 //
